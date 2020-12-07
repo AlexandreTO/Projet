491ac6e907efd7bf564bb77bfcb578a2e6ec4ee8
Alexandre TO <48286489+AlexandreTO@users.noreply.github.com>
2020-12-07 23:50:58 +0100

    Update actions

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,7 +11,7 @@ class TestController
     public function number(): Response
     {
         $number = random_int(0, 100);
-
+        $TesTea = 0;
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
