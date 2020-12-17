03decb32c5a99eab8c07760fa037c7aa4d2c5adf
Loganghilardi <56962676+Loganghilardi@users.noreply.github.com>
2020-12-17 12:28:52 +0100

    #10 entité promotions (#21)

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -11,9 +11,6 @@ class TestController
     public function number(): Response
     {
         $number = random_int(0, 100);
-        $TesTea = 0;
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return new Response('<html><body>Lucky number: ' . $number . '</body></html>');
     }
 }
