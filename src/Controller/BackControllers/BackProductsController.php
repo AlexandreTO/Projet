aaa112abf6d880bdd1770aa74e922b2ccf0c2fc5
Logan <ghilardilogan@gmail.com>
2021-05-25 12:53:29 +0200

    debutCrudProduct

--- src/Controller/BackControllers/BackProductsController.php
+++ src/Controller/BackControllers/BackProductsController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\BackControllers;
 
 use App\Entity\Products;
+use App\Form\ProductFormType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\ProductsRepository;
@@ -19,11 +21,69 @@ class BackProductsController extends AbstractController
 		);
 	}
 
-	/** @Route("/products/{id}", name="view_product") */
+	/** @Route("/back/products/{id}", name="back_view_product") */
 	public function viewProduct(Products $product): Response
 	{
-		return $this->render('products/viewProduct.html.twig', [
+		return $this->render('backOffice/products/viewProduct.html.twig', [
 			'product' => $product,
 		]);
 	}
+
+	   /**
+     * @Route("/back/add-product", name="add_product")
+     */
+    public function addProduct(Request $request): Response
+    {
+		$product = new Products();
+        $form = $this->createForm(ProductFormType::class, $product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+			$entityManager = $this->getDoctrine()->getManager();
+			$entityManager->persist($product);
+			$entityManager->flush();
+        }
+        return $this->render("backOffice/products/addProducts.html.twig", [
+            "form_title" => "Ajouter un produit",
+            "form_product" => $form->createView(),
+        ]);
+    }
+
+	/**
+ 	* @Route("/back/modify-product/{id}", name="modify_product")
+	*/
+
+	public function modifyProduct(Request $request, int $id): Response
+{
+    $entityManager = $this->getDoctrine()->getManager();
+
+    $product = $entityManager->getRepository(Products::class)->find($id);
+    $form = $this->createForm(ProductFormType::class, $product);
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid())
+    {
+        $entityManager->flush();
+		return $this->redirectToRoute('back_list_products');
+    }
+
+    return $this->render("backOffice/products/modifyProduct.html.twig", [
+        "form_title" => "Modifier un produit",
+        "form_product" => $form->createView(),
+    ]);
+}
+
+/**
+ * @Route("/back/delete-product/{id}", name="delete_product")
+ */
+public function deleteProduct(int $id): Response
+{
+    $entityManager = $this->getDoctrine()->getManager();
+    $product = $entityManager->getRepository(Products::class)->find($id);
+    $entityManager->remove($product);
+    $entityManager->flush();
+
+    return $this->redirectToRoute("back_list_products");
+}
 }
---
+++ src/Form/ProductFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Products;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('prix')
+            ->add('image')
+            ->add('status')
+            ->add('quantite')
+            ->add('slug')
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
